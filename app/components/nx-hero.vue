7ec63aa8aff257ac16a41b24ce1d4a10a9c9e7e5
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2025-09-01 15:39:11 +0200

    Update

--- app/components/nx-hero.vue
+++ app/components/nx-hero.vue
@@ -8,7 +8,7 @@ const props = defineProps({
 <template>
   <div class="section">
     <div class="contain">
-      <h1 class="heading heading-xl">{{ props.data.title }}</h1>
+      <h1 class="heading-xl font-display tracking-normal">{{ props.data.title }}</h1>
       <p class="intro mt-6">{{ props.data.description }}</p>
     </div>
   </div>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -12,9 +12,7 @@ const { data: projects } = await useAsyncData('projects', () =>
 
     <section class="section-y">
       <div class="contain">
-
-
-
+        <nx-hero :data="page"></nx-hero>
       </div>
     </section>
   </div>
