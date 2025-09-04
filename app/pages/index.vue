bfc06e761d5885c05a62ab7e557443ade5e815e3
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2025-09-04 22:24:57 +0200

    Update font remove hero

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,6 +6,32 @@ const page = await queryCollection('content').path('/').first();
   <div>
     <nx-meta-tags></nx-meta-tags>
 
-    <nx-hero :data="page"></nx-hero>
+    <div class="lg:main-grid fixed p-contain inset-0">
+      <div class="col-start-2 h-full">
+        <div
+          class="invisible lg:visible absolute top-[calc(var(--spacing-contain)+14px)] font-mono text-[10px] text-fg-secondary tracking-wider"
+        >
+          Netherlands, <nx-time></nx-time>
+        </div>
+
+        <div class="h-full grid items-center">
+          <div>
+            <p class="font-serif text-xl md:text-[28px] text-fg-secondary">
+              {{ page.meta.name }}
+            </p>
+
+            <h1
+              class="mt-1 font-semibold text-[calc(24px+4.5vw)] leading-[1.1] font-display max-w-[18ch]"
+            >
+              {{ page.title }}
+            </h1>
+          </div>
+        </div>
+
+        <p class="absolute bottom-contain text-sm w-[280px] text-fg-secondary">
+          {{ page.description }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
