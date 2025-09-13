46329ce9be6690fb6d9e80de6113d5cff7d49f59
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2025-09-13 22:26:39 +0200

    cleanup

--- app/components/nx-hero.vue
+++ app/components/nx-hero.vue
@@ -75,17 +75,20 @@ onBeforeUnmount(() => {
     >
       {{ data.meta.name || data.meta.period }}
     </p>
+
     <h1
       class="mt-1 font-semibold text-[calc(24px+4.5vw)] leading-[1.1] font-display max-w-[18ch]"
     >
       {{ data.title }}
     </h1>
+
     <p
       v-if="data.meta.descriptionShort"
       class="mt-5 text-fg-secondary text-lg max-w-[40ch] leading-[1.4]"
     >
       {{ data.meta.descriptionShort }}
     </p>
+    
     <div class="mt-8" v-if="data.path">
       <nuxt-link :to="data.path" class="button">Explore</nuxt-link>
     </div>
