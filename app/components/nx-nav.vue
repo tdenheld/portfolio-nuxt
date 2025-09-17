af5ab73a058f3e9016fdd57f543774765ac70310
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2025-09-17 20:19:22 +0200

    update nav

--- app/components/nx-nav.vue
+++ app/components/nx-nav.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <nav class="fixed inset-x-0 z-navigation pt-contain px-contain">
-    <nx-logo class="relative z-raised"></nx-logo>
+  <nav class="fixed inset-x-0 z-navigation pt-contain px-contain lg:main-grid">
+    <div>
+      <nx-logo></nx-logo>
+    </div>
 
     <div
-      class="hidden lg:main-grid fixed inset-x-0 px-contain top-[calc(var(--spacing-contain)+14px)]"
+      class="hidden lg:block col-start-2 absolute top-[calc(var(--spacing-contain)+14px)]"
     >
-      <p class="col-start-2 font-mono text-[10px] text-fg-secondary tracking-wider">
+      <p class="font-mono text-[10px] text-fg-secondary tracking-wider">
         Netherlands, <nx-time></nx-time>
       </p>
     </div>
