11b2ca96e522f49d41eaa7feec19c32699006a22
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2025-09-22 11:12:08 +0200

    fix layout

--- app/pages/projects/[...slug].vue
+++ app/pages/projects/[...slug].vue
@@ -19,18 +19,20 @@ onMounted(() => {
         :image="page.meta.image"
       ></nx-meta-tags>
 
-      <div class="fixed inset-0 main-grid p-contain overflow-y-auto">
-        <div class="col-start-2 h-full">
-          <div class="h-full grid items-center">
+      <div class="fixed inset-0 main-grid px-contain overflow-y-auto no-scrollbar">
+        <div class="col-start-2">
+          <div class="h-[100svh] py-contain grid items-center">
             <nx-hero :data="page" heading-level="h1" pdp></nx-hero>
           </div>
 
-          <div class="-mt-28 space-y-6 a a-fi pb-16">
+          <div class="-mt-28 md:-mt-64 pb-16 space-y-6 a a-fi">
             <nx-image
               v-for="entry in page.meta.items"
               :src="entry.src"
               :alt="entry.alt"
               class="rounded-2xl overflow-hidden"
+              sizes="60vw"
+              :srcset="[320, 640, 1280, 2100]"
             ></nx-image>
           </div>
         </div>
