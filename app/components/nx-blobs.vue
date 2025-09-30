766b7f8a7ef70a77072f0b27cefdd655ae522d89
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2025-09-30 10:52:19 +0200

    update gradients

--- app/components/nx-blobs.vue
+++ app/components/nx-blobs.vue
@@ -2,18 +2,6 @@
 
 <template>
   <div class="fixed inset-0 overflow-hidden pointer-events-none">
-    <div class="gradient-1"></div>
+    <div class="blob"></div>
   </div>
 </template>
-
-<style scoped lang="postcss">
-.gradient-1 {
-  position: absolute;
-  inset: 0;
-  background: radial-gradient(
-    ellipse 20% 50% at 30% 50%,
-    var(--color-bg-secondary) 0%,
-    transparent 100%
-  );
-}
-</style>
