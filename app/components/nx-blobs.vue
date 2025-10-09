3047d6709f14bd60595b95483cb51dd7bd40dd5c
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2025-10-09 12:33:33 +0200

    Remove blobs when low framerate

--- app/components/nx-blobs.vue
+++ app/components/nx-blobs.vue
@@ -1,10 +1,9 @@
 <script setup>
 const fps = ref(0);
 const showFps = ref(false);
+const showBlobs = ref(true);
 
 const calculateFps = () => {
-  if (!showFps.value) return;
-
   fps.value = 1;
   const times = [];
 
@@ -15,6 +14,9 @@ const calculateFps = () => {
 
     times.push(timestamp);
     fps.value = times.length;
+
+    showBlobs.value = fps.value >= 52;
+
     requestAnimationFrame(fpsLoop);
   };
 
@@ -28,18 +30,21 @@ onMounted(() => {
 
 <template>
   <div>
-    <div class="fixed inset-0 overflow-hidden pointer-events-none">
+    <div v-if="showBlobs" class="fixed inset-0 overflow-hidden pointer-events-none">
       <div class="blob blob--1"></div>
       <div class="blob blob--2"></div>
+    </div>
 
-      <p v-if="showFps" class="font-mono text-[10px] pt-0.5 pl-1">
-        {{ fps }}
-      </p>
+    <div v-if="showFps" class="font-mono text-[10px] fixed top-0.5 left-1">
+      {{ fps }}
     </div>
 
     <div
       class="fixed bottom-0 left-0 p-6 z-navigation"
-      @click="showFps = !showFps; calculateFps()"
+      @click="
+        showFps = !showFps;
+        calculateFps();
+      "
     ></div>
   </div>
 </template>
