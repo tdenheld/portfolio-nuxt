d8f6a770464b2b3455f44981642d419ad8ed66fb
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2025-10-09 19:44:30 +0200

    update fps

--- app/plugins/runPerformanceCheck.ts
+++ app/plugins/runPerformanceCheck.ts
@@ -3,7 +3,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     return new Promise<boolean>((resolve) => {
       const times: number[] = [];
       const fpsHistory: number[] = [];
-      const FPS_THRESHOLD = 50; // fps threshold to show blobs
+      const FPS_THRESHOLD = 52; // fps threshold to show blobs
       const STABILITY_THRESHOLD = 5; // fps variance allowed
       const MIN_SAMPLES = 10; // minimum readings before checking stability
       let fps: number;
