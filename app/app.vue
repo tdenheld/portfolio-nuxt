b99ca8cf64735a3b31963f85b296ead115d099e4
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2025-10-13 11:11:55 +0200

    remove moving gradients

--- app/app.vue
+++ app/app.vue
@@ -1,15 +1,3 @@
-<script setup>
-const nuxtApp = useNuxtApp();
-const isPerformant = useState('isPerformant', () => false);
-
-onBeforeMount(async () => {
-  // Only run the performance check if not already set
-  if (isPerformant.value === false) {
-    isPerformant.value = await nuxtApp.$runPerformanceCheck();
-  }
-});
-</script>
-
 <template>
   <nuxt-layout>
     <nuxt-page></nuxt-page>
--- app/components/nx-blobs.vue
+++ app/components/nx-blobs.vue
@@ -1,11 +1,6 @@
-<script setup>
-const isPerformant = useState('isPerformant');
-</script>
-
 <template>
   <div>
     <div
-      v-if="isPerformant"
       class="fixed inset-0 overflow-hidden pointer-events-none a-fi [animation-duration:3s]"
     >
       <div class="blob blob--1"></div>
