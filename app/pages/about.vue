8dfaeea5af6a5577846c2512e08bef9d31a96107
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2025-10-24 15:20:26 +0200

    add experience

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -14,21 +14,33 @@ onMounted(() => {
     <div class="pt-32 lg:pt-64 px-contain lg:main-grid">
       <h1 class="sr-only">{{ page.title }}</h1>
 
-      <div class="col-start-2 col-span-2 md:flex md:items-center">
-        <nx-image
-          :src="page.meta.image"
-          :alt="page.title"
-          class="overflow-hidden rounded-4xl md:max-w-lg grow aspect-square"
-          sizes="(min-width: 90rem) 32rem, (min-width: 61.25rem) 33vw, (min-width: 46.25rem) 40vw, 80vw"
-          :srcset="[320, 640, 1280]"
-          preload
-        ></nx-image>
-
-        <p
-          class="relative mt-8 md:mt-0 md:-ml-12 lg:-ml-16 max-w-[38ch] text-fg-secondary text-base md:text-lg xl:text-xl"
+      <div class="col-start-2 col-span-2">
+        <div
+          class="md:grid md:items-center md:grid-cols-2 lg:grid-cols-[2fr_3fr] xl:grid-cols-[3fr_5fr]"
         >
-          {{ page.description }}
-        </p>
+          <nx-image
+            :src="page.meta.image"
+            :alt="page.title"
+            class="overflow-hidden rounded-4xl aspect-square"
+            sizes="(min-width: 90rem) 32rem, (min-width: 61.25rem) 33vw, (min-width: 46.25rem) 40vw, 80vw"
+            :srcset="[320, 640, 1280]"
+            preload
+          ></nx-image>
+
+          <div class="relative mt-8 md:mt-0 md:-ml-12 lg:-ml-16">
+            <p class="max-w-[38ch] text-fg-secondary text-base md:text-lg xl:text-xl">
+              {{ page.description }}
+            </p>
+          </div>
+        </div>
+
+        <div
+          class="mt-16 md:grid md:items-center md:grid-cols-2 lg:grid-cols-[2fr_3fr] xl:grid-cols-[3fr_5fr]"
+        >
+          <div class="col-start-2 relative mt-8 md:mt-0 md:-ml-12 lg:-ml-16">
+            <h2 class="font-mono text-xs uppercase tracking-widest">Experience</h2>
+          </div>
+        </div>
       </div>
     </div>
   </div>
