48b142426ff2d6e890a3ee8dacbec416cd162ebb
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2025-10-24 15:55:43 +0200

    Add fixed container

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -8,52 +8,56 @@ onMounted(() => {
 </script>
 
 <template>
-  <div v-if="page">
+  <div>
     <nx-meta-tags :title="page.title" :description="page.description"></nx-meta-tags>
 
-    <div class="pt-32 lg:pt-64 px-contain lg:main-grid">
-      <h1 class="sr-only">{{ page.title }}</h1>
+    <div class="fixed inset-0 overflow-y-auto no-scrollbar">
+      <div class="pt-32 lg:pt-64 px-contain lg:main-grid">
+        <h1 class="sr-only">{{ page.title }}</h1>
 
-      <div class="col-start-2 col-span-2">
-        <div class="about-grid">
-          <nx-image
-            :src="page.meta.image"
-            :alt="page.title"
-            class="overflow-hidden rounded-4xl aspect-square"
-            sizes="(min-width: 90rem) 32rem, (min-width: 61.25rem) 33vw, (min-width: 46.25rem) 40vw, 80vw"
-            :srcset="[320, 640, 1280]"
-            preload
-          ></nx-image>
+        <div class="col-start-2 col-span-2">
+          <div class="about-grid">
+            <nx-image
+              :src="page.meta.image"
+              :alt="page.title"
+              class="overflow-hidden rounded-4xl aspect-square"
+              sizes="(min-width: 90rem) 32rem, (min-width: 61.25rem) 33vw, (min-width: 46.25rem) 40vw, 80vw"
+              :srcset="[320, 640, 1280]"
+              preload
+            ></nx-image>
 
-          <div class="relative mt-8 md:mt-0 md:-ml-12 lg:-ml-16">
-            <p class="max-w-[38ch] text-fg-secondary text-base md:text-lg xl:text-xl">
-              {{ page.description }}
-            </p>
+            <div class="relative mt-8 md:mt-0 md:-ml-12 lg:-ml-16">
+              <p
+                class="max-w-[38ch] text-fg-secondary text-base md:text-lg xl:text-xl"
+              >
+                {{ page.description }}
+              </p>
+            </div>
           </div>
-        </div>
 
-        <div class="my-16 about-grid">
-          <div class="col-start-2 relative mt-8 md:mt-0 md:-ml-12 lg:-ml-16">
-            <h2 class="font-mono text-xs uppercase tracking-widest">Experience</h2>
+          <div class="my-16 about-grid">
+            <div class="col-start-2 relative mt-8 md:mt-0 md:-ml-12 lg:-ml-16">
+              <h2 class="font-mono text-xs uppercase tracking-widest">Experience</h2>
 
-            <div
-              v-for="entry in page.meta.experience"
-              :key="entry.title"
-              class="mt-6"
-            >
-              <h3 class="text-fg-secondary text-sm">
-                {{ entry.title }} ({{ entry.period }})
-              </h3>
+              <div
+                v-for="entry in page.meta.experience"
+                :key="entry.title"
+                class="mt-6"
+              >
+                <h3 class="text-fg-secondary text-sm">
+                  {{ entry.title }} ({{ entry.period }})
+                </h3>
 
-              <ul>
-                <li
-                  class="text-xs font-mono italic mt-0.5"
-                  v-for="role in entry.roles"
-                  :key="role"
-                >
-                  {{ role }}
-                </li>
-              </ul>
+                <ul>
+                  <li
+                    class="text-xs font-mono italic mt-0.5"
+                    v-for="role in entry.roles"
+                    :key="role"
+                  >
+                    {{ role }}
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
