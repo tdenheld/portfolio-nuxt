ebb5f7d3c209a4fe35b59c9f8d27f10416d79814
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2025-10-27 22:50:50 +0100

    Add vid vpro

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -17,30 +17,26 @@ onMounted(() => {
 
         <div class="col-start-2 col-span-2">
           <div class="about-grid">
+            <div class="md:order-last relative mb-8 md:mb-0 md:-ml-12 lg:-ml-16">
+              <p class="max-w-[38ch] text-fg-secondary text-lg xl:text-xl">
+                {{ page.description }}
+              </p>
+            </div>
+
             <nx-image
               :src="page.meta.image"
               :alt="page.title"
-              class="overflow-hidden rounded-4xl aspect-square"
+              class="max-w-md md:max-w-[initial] overflow-hidden rounded-4xl aspect-square"
               sizes="(min-width: 90rem) 32rem, (min-width: 61.25rem) 33vw, (min-width: 46.25rem) 40vw, 80vw"
               :srcset="[320, 640, 1280]"
               preload
             ></nx-image>
-
-            <div class="relative mt-8 md:mt-0 md:-ml-12 lg:-ml-16">
-              <p
-                class="max-w-[38ch] text-fg-secondary text-base md:text-lg xl:text-xl"
-              >
-                {{ page.description }}
-              </p>
-            </div>
           </div>
 
-          <div class="my-24 about-grid items-start">
+          <div class="mt-12 mb-14 md:my-24 about-grid items-start">
             <div class="relative mt-8 md:mt-0">
               <div>
-                <h2 class="font-mono text-xs uppercase tracking-[0.16em]">
-                  Brands
-                </h2>
+                <h2 class="font-mono text-xs uppercase tracking-[0.16em]">Brands</h2>
 
                 <p class="mt-4 pr-32 max-w-[48ch] text-fg-secondary text-sm">
                   <template v-for="(item, index) in page.meta.brands" :key="item">
@@ -68,16 +64,13 @@ onMounted(() => {
               </div>
             </div>
 
-            <div class="relative mt-16 md:mt-0 md:-ml-12 lg:-ml-16">
+            <div class="relative mt-12 md:mt-0 md:-ml-12 lg:-ml-16">
               <h2 class="font-mono text-xs uppercase tracking-[0.16em]">
                 Experience
               </h2>
 
               <div class="mt-4 space-y-5">
-                <div
-                  v-for="entry in page.meta.experience"
-                  :key="entry.title"
-                >
+                <div v-for="entry in page.meta.experience" :key="entry.title">
                   <h3 class="text-fg-secondary text-sm">
                     {{ entry.title }}
                     <span class="whitespace-nowrap">({{ entry.period }})</span>
