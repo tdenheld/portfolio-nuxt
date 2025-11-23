1fec5eb7cc0ad5c22284050d9edb7fee1aa65e99
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2025-11-23 15:45:39 +0100

    update lug

--- app/pages/projects/[...slug].vue
+++ app/pages/projects/[...slug].vue
@@ -93,14 +93,15 @@ onMounted(() => {
               </div>
             </div>
 
-            <div class="lg:py-contain">
-              <h2 class="font-display font-semibold text-xl md:text-3xl">
+            <div class="max-lg:pt-16 lg:py-contain">
+              <h2 class="font-display font-semibold text-2xl md:text-3xl">
                 More work
               </h2>
-              <ul class="mt-8 space-y-2">
+
+              <ul class="mt-3 space-y-1">
                 <li v-for="entry in otherProjects" :key="entry.path">
                   <nuxt-link
-                    class="text-fg-secondary link"
+                    class="text-fg-secondary link text-[10px] md:text-xs uppercase tracking-[0.16em]"
                     :key="entry.path"
                     :to="entry.path"
                   >
