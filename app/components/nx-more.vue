81790abd37bd63468a549b74da2e2da3a44c6e3d
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2025-11-23 15:52:55 +0100

    Add more-work-comp

---
+++ app/components/nx-more.vue
@@ -0,0 +1,26 @@
+<script setup>
+const props = defineProps({
+  projects: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="max-lg:pt-16 lg:py-contain">
+    <h2 class="font-display font-semibold text-2xl md:text-3xl">More work</h2>
+
+    <ul class="mt-3 space-y-1">
+      <li v-for="entry in projects" :key="entry.path">
+        <nuxt-link
+          class="text-fg-secondary link text-[10px] md:text-xs uppercase tracking-[0.16em]"
+          :key="entry.path"
+          :to="entry.path"
+        >
+          <span>{{ entry.title }}</span>
+        </nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
--- app/pages/projects/[...slug].vue
+++ app/pages/projects/[...slug].vue
@@ -93,23 +93,7 @@ onMounted(() => {
               </div>
             </div>
 
-            <div class="max-lg:pt-16 lg:py-contain">
-              <h2 class="font-display font-semibold text-2xl md:text-3xl">
-                More work
-              </h2>
-
-              <ul class="mt-3 space-y-1">
-                <li v-for="entry in otherProjects" :key="entry.path">
-                  <nuxt-link
-                    class="text-fg-secondary link text-[10px] md:text-xs uppercase tracking-[0.16em]"
-                    :key="entry.path"
-                    :to="entry.path"
-                  >
-                    <span>{{ entry.title }}</span>
-                  </nuxt-link>
-                </li>
-              </ul>
-            </div>
+            <nx-more :projects="otherProjects"></nx-more>
           </div>
         </div>
       </div>
