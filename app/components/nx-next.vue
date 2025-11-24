faacfb86da9a9301b229581abc81834ae4b0b2c0
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2025-11-24 20:21:32 +0100

    Add next size

--- app/components/nx-next.vue
+++ app/components/nx-next.vue
@@ -8,14 +8,18 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="pt-24 pb-2 lg:pt-contain lg:pb-32">
-    <nuxt-link :to="project.path" class="cursor-pointer touch-manipulation">
+  <div class="pt-24 pb-2 lg:pt-32 lg:pb-48">
+    <nuxt-link
+      :to="project.path"
+      class="group cursor-pointer touch-manipulation inline-block"
+    >
       <h2
-        class="font-display font-[150] hover:font-[700] transition-all duration-600 text-[calc(2rem+12vw)] leading-[0.8]"
+        class="font-display font-[150] group-hover:font-[700] transition-all duration-600 text-[calc(3rem+10vw)] leading-[0.8]"
       >
         Next
       </h2>
-      <p class="font-serif md:text-2xl md:-mt-4 text-fg-secondary">
+
+      <p class="text-sm md:text-base md:-mt-4 text-fg-secondary">
         {{ project.title }}
       </p>
     </nuxt-link>
