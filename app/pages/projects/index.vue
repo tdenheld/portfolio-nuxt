8f6a8924da2d9a4d8134ca210ca471dbf3d6c71d
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2026-02-19 20:14:09 +0100

    update layout

--- app/pages/projects/index.vue
+++ app/pages/projects/index.vue
@@ -13,17 +13,29 @@ onMounted(() => {
 
     <h1 class="sr-only">Selected Work</h1>
 
-    <div class="s-carousel no-scrollbar">
-      <div class="grid justify-center py-32">
-        <div class="grid grid-cols-2 gap-x-[12vw] gap-y-16">
-          <nuxt-link v-for="entry in projects" :key="entry.title" :to="entry.path" class="even:mt-56">
-            <h2>
-              {{ entry.title }}
-            </h2>
+    <div class="s-scroller no-scrollbar">
+      <div class="grid justify-center pb-32">
+        <div class="grid grid-cols-2 gap-8 lg:gap-16 xl:gap-x-[12vw] gap-y-16">
+          <div class="hidden lg:block"></div>
+
+          <nuxt-link
+            v-for="entry in projects"
+            :key="entry.title"
+            :to="entry.path"
+            class="flex flex-col-reverse gap-6 group lg:even:mt-48 lg:flex-row lg:even:flex-row-reverse odd:justify-self-end even:justify-self-start"
+          >
+            <div class="max-w-3xs group-odd:text-right relative lg:-top-[3px]">
+              <h2 class="font-display text-3xl font-[850]">
+                {{ entry.title }}
+              </h2>
+
+              <p class="mt-2 text-sm text-fg-secondary/70 leading-normal">{{ entry.meta.descriptionShort }}</p>
+            </div>
+
             <nx-image
               :src="entry.meta.image"
               alt=""
-              image-class="w-[15rem] aspect-[5/3] rounded-lg object-cover"
+              image-class="w-[15rem] aspect-[7/4] rounded-lg object-cover"
               sizes="15rem"
               :srcset="[320, 640, 1280, 1600]"
               :preload="true"
@@ -36,7 +48,7 @@ onMounted(() => {
 </template>
 
 <style scoped lang="postcss">
-.s-carousel {
+.s-scroller {
   position: fixed;
   inset: 0;
   padding: var(--spacing-contain);
