1f09e9eef3d1df86d3881fb92384ac0217aa9201
Tibor den Held <41893505+tdenheld@users.noreply.github.com>
2026-03-31 15:59:58 +0200

    Update head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,20 @@ export default defineNuxtConfig({
     pageTransition: { name: 'page' },
 
     head: {
+      title: 'Tibor den Held – Design Engineer & Creative Developer',
+      meta: [
+        {
+          name: 'description',
+          content:
+            'Tibor crafts scalable and accessible experiences. He specializes in design systems, product design and engineering.',
+        },
+      ],
+      style: [
+        {
+          innerHTML: `html{background-color:#232927}.p-grid{display:grid;place-content:center;position:fixed;inset:0;width:100%;height:100%;box-sizing:border-box}.p-logo{position:relative;display:grid;place-content:center;width:80px;height:80px}.p-logo:before{content:'';position:absolute;inset:0;background-color:#3c796b;border-radius:12px;animation:rotate 1.5s linear infinite}.p-logo svg{position:relative;padding-bottom:4px;color:#cdd388;width:40px}@keyframes rotate{from{transform:rotate(0deg)}to{transform:rotate(360deg)}}`,
+        },
+      ],
+
       link: [
         {
           rel: 'apple-touch-icon',
